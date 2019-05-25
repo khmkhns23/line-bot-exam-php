70c2bcbb332f1801380989849dcb38e79f043e9e
khmkhns23 <33107078+khmkhns23@users.noreply.github.com>
2019-05-25 23:24:08 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -2,7 +2,7 @@
 $access_token = '4/Q8KZY2YlyxmdY3VY8e7DvM3V4U6hvR9D1kqX3sF/JMwGbfLTkqiyLZbCu2KHbGGYTEhJ5L3Zy3I132J4OtT6mJifdN4iNPBPHXSszB33xdCica5EnUOas9WVoJE2BtEbPQgmffXkOOv7mL/jPaxwdB04t89/1O/w1cDnyilFU=';
 
 
-$url = 'https://api.line.me/v2/oauth/verify';
+$url = 'https://api.line.me/v2/bot/message/reply';
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
